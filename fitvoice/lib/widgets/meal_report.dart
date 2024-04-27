79fa2755e162f60245446aeb88c55954183cefcc
Kemjiga <kemerj@uninorte.edu.co>
2024-04-27 03:16:21 -0500

    fix: change init params

--- fitvoice/lib/widgets/meal_report.dart
+++ fitvoice/lib/widgets/meal_report.dart
@@ -1,45 +1,15 @@
-import 'package:fitvoice/models/food_items_model.dart';
+//import 'package:fitvoice/models/food_items_model.dart';
 import 'package:flutter/material.dart';
 import 'package:fitvoice/screens/meal_review_screen.dart';
 
 import 'package:fitvoice/models/meal_report_model.dart';
 
 class MealReportCard extends StatelessWidget {
-  const MealReportCard(
-      {super.key,
-      required this.id,
-      required this.appUserId,
-      required this.audioId,
-      required this.rawTranscript,
-      required this.foodReports,
-      required this.dbLookupPreference,
-      required this.mealRecordedAt,
-      required this.pending,
-      required this.reviewed});
-
-  final String id;
-  final String appUserId;
-  final String audioId;
-  final String rawTranscript;
-  final List<FoodItemsModel> foodReports;
-  final String dbLookupPreference;
-  final DateTime mealRecordedAt;
-  final bool pending;
-  final bool reviewed;
-
-  MealReportModel get mealReport {
-    return MealReportModel(
-      id: id,
-      appUserId: appUserId,
-      audioId: audioId,
-      rawTranscript: rawTranscript,
-      foodReports: foodReports,
-      dbLookupPreference: dbLookupPreference,
-      mealRecordedAt: mealRecordedAt,
-      pending: pending,
-      reviewed: reviewed,
-    );
-  }
+  const MealReportCard({
+    super.key,
+    required this.mealReport,
+  });
+  final MealReportModel mealReport;
 
   @override
   Widget build(BuildContext context) {
@@ -56,10 +26,10 @@ class MealReportCard extends StatelessWidget {
           },
           child: ListTile(
             title: Text(
-              'Reporte #$id',
+              'Reporte #${mealReport.id}',
               style: const TextStyle(fontWeight: FontWeight.w400),
             ),
-            subtitle: Text(rawTranscript),
+            subtitle: Text(mealReport.rawTranscript),
           ),
         ),
       ),
