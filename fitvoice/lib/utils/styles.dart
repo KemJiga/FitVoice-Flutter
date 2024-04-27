682f75f7391822937d11d87be11bd7d8ee387932
Kemjiga <kemerj@uninorte.edu.co>
2024-04-27 03:17:02 -0500

    feat: add style class to globaly controll styles

---
+++ fitvoice/lib/utils/styles.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Estilos {
+  static const estiloTexto = TextStyle(
+    fontSize: 30,
+    color: Colors.black,
+  );
+  static const color1 = Color.fromRGBO(46, 209, 46, 1);
+  static const color2 = Color.fromRGBO(255, 169, 53, 1);
+}
