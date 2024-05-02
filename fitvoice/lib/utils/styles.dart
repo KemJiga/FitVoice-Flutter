39efb7234c7d4d22f14d064d2092eb1112738042
Kemjiga <kemerj@uninorte.edu.co>
2024-05-01 22:08:52 -0500

    fix: implement font

--- fitvoice/lib/utils/styles.dart
+++ fitvoice/lib/utils/styles.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 
 class Estilos {
-  static const estiloTexto = TextStyle(
-    fontSize: 30,
-    color: Colors.black,
-  );
   //static const color1 = Color.fromRGBO(46, 209, 46, 1);
   //static const color2 = Color.fromRGBO(255, 169, 53, 1);
   static const color1 = Color.fromRGBO(0, 193, 162, 1);
@@ -12,4 +8,14 @@ class Estilos {
   static const color3 = Color.fromRGBO(215, 178, 157, 1);
   static const color4 = Color.fromRGBO(203, 133, 137, 1);
   static const color5 = Color.fromRGBO(121, 100, 101, 1);
+  static const fontFamily = 'BrandonGrotesque';
+
+  static TextStyle textStyle1(double size, Color color, String type) {
+    return TextStyle(
+      color: color,
+      fontFamily: fontFamily,
+      fontSize: size,
+      fontWeight: type == 'bold' ? FontWeight.bold : FontWeight.normal,
+    );
+  }
 }
