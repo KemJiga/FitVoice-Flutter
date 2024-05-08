161456cad8d37759df162c0ec1295dd7d93beaf7
Kemjiga <kemerj@uninorte.edu.co>
2024-05-08 01:42:03 -0500

    feat: create user report model

---
+++ fitvoice/lib/models/user_report_model.dart
@@ -0,0 +1,13 @@
+class UserReportModel {
+  UserReportModel({
+    required this.foodName,
+    required this.description,
+    required this.amount,
+    required this.unit,
+  });
+
+  String foodName;
+  String description;
+  double amount;
+  String unit;
+}
