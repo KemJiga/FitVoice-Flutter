aa2ba02cf5670149ea63367f6885fcebf2b2fb29
Kemjiga <kemerj@uninorte.edu.co>
2024-05-20 19:32:55 -0500

    fix: change color pallete

--- fitvoice/lib/utils/styles.dart
+++ fitvoice/lib/utils/styles.dart
@@ -3,11 +3,13 @@ import 'package:flutter/material.dart';
 class Estilos {
   //static const color1 = Color.fromRGBO(46, 209, 46, 1);
   //static const color2 = Color.fromRGBO(255, 169, 53, 1);
-  static const color1 = Color.fromRGBO(0, 193, 162, 1);
+  //static const color1 = Color.fromRGBO(0, 193, 162, 1);
   static const color2 = Color.fromRGBO(232, 210, 174, 1);
   static const color3 = Color.fromRGBO(215, 178, 157, 1);
   static const color4 = Color.fromRGBO(203, 133, 137, 1);
-  static const color5 = Color.fromRGBO(121, 100, 101, 1);
+  static const color1 = Color.fromRGBO(91, 123, 122, 1);
+  //static const color1 = Color.fromRGBO(121, 100, 101, 1);
+  static const color5 = Colors.black;
   static const fontFamily = 'BrandonGrotesque';
 
   static TextStyle textStyle1(double size, Color color, String type) {
